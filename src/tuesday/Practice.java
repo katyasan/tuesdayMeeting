3390020989e78dd2258edab8200f7edead57bbf8
DzhulAleks <dzhulietta.ua@gmail.com>
2019-12-03 13:19:31 -0600

    new changes

--- src/tuesday/Practice.java
+++ src/tuesday/Practice.java
@@ -1,11 +1,17 @@
 package tuesday;
 
 public class Practice {
+
     public static void main(String[] args)
     {
         System.out.println("sgfjfgjh");
 
     System.out.println("Hi,guys");
-}
+
+        int x = 9;
+        System.out.println("Hello again");
+        System.out.println(x);
+    }
+
 }
 
