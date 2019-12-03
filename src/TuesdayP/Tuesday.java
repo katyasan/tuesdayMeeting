39a9af152375d0a78835f0fe12d6652100197021
Nurgul duishebaeva <duishebaevanuku@gamil.com>
2019-12-03 13:39:38 -0600

    nurgul added

---
+++ src/TuesdayP/Tuesday.java
@@ -0,0 +1,16 @@
+package TuesdayP;
+
+public class Tuesday {
+    public static void main(String[] args) {
+
+
+        System.out.println("Happy Tuesday" +
+                "");
+        System.out.println("happy again");
+    }
+
+
+
+
+
+}
