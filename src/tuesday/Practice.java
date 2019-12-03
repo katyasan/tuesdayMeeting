fa05239260cb3cd4855dd0a53933f8af6416b9db
admin <12hleb@gmail.com>
2019-12-03 13:34:06 -0600

    by gleb

--- src/tuesday/Practice.java
+++ src/tuesday/Practice.java
@@ -3,6 +3,7 @@ package tuesday;
 public class Practice {
     public static void main(String[] args) {
 
+<<<<<<< Updated upstream
         System.out.println("sgfjfgjh");
         System.out.println("Kanykei is.....");
 
@@ -15,6 +16,13 @@ public class Practice {
         int x = 9;
         System.out.println("Hello again");
         System.out.println(x);
+=======
+        System.out.println("sgfjfgjh");
+
+    }
+    public void Gleb(){
+        System.out.println("Gleb");
+>>>>>>> Stashed changes
     }
 
 }
