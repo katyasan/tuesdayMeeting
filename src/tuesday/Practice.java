f9f1a2d772a76a47a9badad139c3ca9b0d225549
kanykeiapsalbekova <apasalbekovakanykei@gmail.com>
2019-12-03 11:05:09 -0800

    Message

--- src/tuesday/Practice.java
+++ src/tuesday/Practice.java
@@ -2,6 +2,9 @@ package tuesday;
 
 public class Practice {
     public static void main(String[] args) {
+
         System.out.println("sgfjfgjh");
+        System.out.println("Kanykei is.....");
+
     }
 }
